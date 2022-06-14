380d26e8b2ae234dbdd1250906e7cf36451d614b
Ubuntu <ubuntu@RPi3B.cogeco.local>
2022-06-14 01:46:42 +0000

    Base project finished

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from speak import speak
 from googletrans import Translator
 
-translator = Translator()
-count = 0
-
-translated_text = translator.translate("Traduis ce texte")
-print(translated_text.text)
-speak(translated_text.text)
+def run(preText):
+    translator = Translator()
+    count = 0
+    translated_text = translator.translate(preText)
+    print(translated_text.text)
+    speak(translated_text.text)
